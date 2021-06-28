91cd9c967ee832565555663ae91652e6214bbe9b
Lucas-Sholtz <misteriphone7.7.7@gmail.com>
2021-06-28 14:54:32 +0300

    README, comments, minor changes

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # btcrateapp
  Simple WEB API for viewing BTC-UAH rate
+
+1. In command line change directory to app folder with command "cd %app_folder_path%
+2. Run the app using "nodemon app.js" or "node app.js"
+3. Enter http://localhost:3000/ into your address bar
+4. You're on the /user/create, here you can register in app via email
+5. After successful registration proceed to /user/login and log-in using email and password
+6. After log-in go to /btcrate to view the BTC-UAH rate
--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const hbs = require("hbs");
 const bcrypt = require("bcrypt");
   
 const app = express();
-const jsonParser = express.json();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const usersPath = "users.json"; 
 const tokenPath = "users_tokens.json";
@@ -16,15 +15,19 @@ const settings = { method: "Get" };
 
 app.set("view engine", "hbs");
 hbs.registerPartials(__dirname + "/views/partials");
+
 app.use(cookieParser());
 
-app.get("/user/create", urlencodedParser, function (request, response) {
+app.get("/", function (request, response) {
+    response.redirect("/user/create");
+});
+
+app.get("/user/create", function (request, response) { //on /user/create GET simply render page
     response.render("create.hbs");
 });
 
-app.post("/user/create", urlencodedParser, function (request, response) {
-    if(!request.body) return response.sendStatus(400);
-    console.log(request.body);
+app.post("/user/create", urlencodedParser, function (request, response) { //on /user/create POST
+    if(!request.body) return response.sendStatus(400); //check for empty body
 
     let newEmail = request.body.userEmail;
     let newPassword = request.body.userPassword;
@@ -35,27 +38,27 @@ app.post("/user/create", urlencodedParser, function (request, response) {
     let users = JSON.parse(data);
 
     let exists = false;
-    for (let i = 0; i < users.length; i++) {
+    for (let i = 0; i < users.length; i++) { //check database
         if (users[i].email == newUser.email) {
             exists = true;
         }
     }
 
     if (exists) {
-        response.send("User already exists!");
+        response.send("User already exists!"); //user cannot register to the same mail 
     } else {
-        users.push(newUser);
-        fs.writeFileSync(usersPath, JSON.stringify(users), "utf-8");
+        users.push(newUser); //add new user to array
+        fs.writeFileSync(usersPath, JSON.stringify(users), "utf-8"); //and write it to database
         response.send(`Email: ${newEmail}<br> Password: ${newPassword}<br><br>Succesfully created!<br><a href="/user/login">Login</a>`);
     }
 })
 
-app.get("/user/login", urlencodedParser, function (request, response) {
+app.get("/user/login", function (request, response) { //on /user/login GET simply render page
     response.render("login.hbs");
 });
 
 app.post("/user/login", urlencodedParser, function (request, response) {
-    if (!request.body) return response.sendStatus(400);
+    if (!request.body) return response.sendStatus(400); //check for empty body
 
     let userEmail = request.body.userEmail;
     let userPassword = request.body.userPassword;
@@ -63,68 +66,70 @@ app.post("/user/login", urlencodedParser, function (request, response) {
     let data = fs.readFileSync(usersPath, "utf-8");
     let users = JSON.parse(data);
 
-    for (let i = 0; i < users.length; i++) {
+    for (let i = 0; i < users.length; i++) { //find user with same email 
         if (users[i].email == userEmail) {
-            if (bcrypt.compareSync(userPassword, users[i].password)) {
-                let newToken = bcrypt.genSaltSync();
+            if (bcrypt.compareSync(userPassword, users[i].password)) { //compare passwords
+                let newToken = bcrypt.genSaltSync(); //generate unique token for user
                 let tokenData = fs.readFileSync(tokenPath);
                 let userTokens = JSON.parse(tokenData);
 
                 let exists = false;
-                for (let j = 0; j < userTokens.length; j++) {
+                for (let j = 0; j < userTokens.length; j++) { //check database
                     if (userTokens[j].email == userEmail) {
                         exists = true;
-                        userTokens[i].token = newToken;
+                        userTokens[i].token = newToken; //if it already exists, change data
                     }
                 }
-                if (!exists) {
+                if (!exists) { //if not, add it to array
                     let userToken = { email: userEmail, token: newToken };
                     userTokens.push(userToken);
                 }
-                fs.writeFileSync(tokenPath, JSON.stringify(userTokens), "utf-8");
-                response.cookie("token", newToken, { maxAge: 90000 })
-                    .cookie("email", userEmail, { maxAge: 90000 })
+                fs.writeFileSync(tokenPath, JSON.stringify(userTokens), "utf-8"); //write database
+
+                response.cookie("token", newToken, { maxAge: 90000 }) //unique token for login stored in cookies
+                    .cookie("email", userEmail, { maxAge: 90000 }) //logged user email stored in cookies
                     .send("Succesfully logged in <br><a href=\"/btcrate\">View rate</a>");
             } else {
-                response.send("Incorrect email or password");
+                response.send("Incorrect email or password"); //user entered wrong password
             }
         }
     }
-    response.send("User does not exist");
+    response.send("User does not exist"); //user tryed to login with non-existent email
 });
 
-app.get("/btcrate", function (request, response) {
+app.get("/btcrate", function (request, response) { //on /btcrate GET
+    //parse cookies
     let email = request.cookies["email"];
     let token = request.cookies["token"];
 
     if (email == undefined || token == undefined) {
-        response.render("accessdenied.hbs");
+        response.render("accessdenied.hbs"); //denie access if cookies empty
     }
 
     let data = fs.readFileSync(tokenPath);
     let userTokens = JSON.parse(data);
 
     let correct = false;
-    for (let i = 0; i < userTokens.length; i++) {
-        if (userTokens[i].email == email) {
-            if (userTokens[i].token == token)
+    for (let i = 0; i < userTokens.length; i++) { //check database
+        if (userTokens[i].email == email) { //find email
+            if (userTokens[i].token == token) //and compare tokens
                 correct = true;
         }
     }
 
-    if (correct)
+    if (correct) 
     {
-        fetch(btcrateUrl, settings)
+        fetch(btcrateUrl, settings) //get BTC rate JSON from api.coindesk.com
             .then(res => res.json())
             .then((json) => {
-                response.render("btcrate.hbs", {
+                response.render("btcrate.hbs", { //render rate page and replace placeholders
                     btcRate: json.bpi.UAH.rate,
                     time: json.time.updated
                 });
             });
     } else {
-        response.render("accessdenied.hbs");
+        response.render("accessdenied.hbs"); //incorrect email or token
     }
 });
 
-app.listen(3000);
+app.listen(3000); //app runs on http://localhost:3000
